1a3f84d1f46a7bcc7dc7c8e9c84e99211abd523b
Reuel <firmwarajak08@gmail.com>
2026-05-17 15:55:33 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="bg-sky-900 h-screen flex items-center justify-center">
+      <h1>oie</h1>
+    </div>
+  );
+}
